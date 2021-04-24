03281598f326e3e19d430330e995c221878024c4
heoseongh <shheo0807@gmail.com>
2021-04-24 16:43:51 +0900

    공모전 CRUD API v1.0

--- contest/contest_api/contestdto.py
+++ contest/contest_api/contestdto.py
@@ -2,6 +2,8 @@ from rest_framework import serializers, viewsets
 from .models import Contest
 
 
+# fields ='__all__' * 모든 필드를 JSON 데이터에 포함시킨다.
+# JSON 데이터에 담길 내용을 커스텀하려면 아래의 주석처럼 필드를 입맛에 맞게 선언해주면 된다.
 class ContestSerializer(serializers.ModelSerializer):
 
     class Meta:
